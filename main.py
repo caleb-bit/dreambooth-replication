86ad7118ce96d813e07c5e37e1c380c04f68bc4f
yottayocta <chubbean.rq@gmail.com>
2026-04-30 02:01:38 -0400

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+from train import train
+from eval import eval
+from generate import generate
+
+
+def build_parser():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command", required=True)
+    subparsers.add_parser("train")
+    subparsers.add_parser("eval")
+    subparsers.add_parser("generate")
+    return parser
+
+
+if __name__ == "__main__":
+    args = build_parser().parse_args()
+    {"train": train, "eval": eval, "generate": generate}[args.command]()
